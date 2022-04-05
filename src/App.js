72e4372b07042d7d7d58a163d2c2bcb0bc0fdc19
benharlucas <benhar.masahod@msugensan.edu.ph>
2022-04-05 13:54:03 +0800

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
-import Footer from './componnets/Footer'
+import Footer from './components/Footer'
 function App() {
   return (
     <>
     <Header />
-    <h1>Grade 5 Task Hihi</h1>
+    <h1>Grade 5 Task! Hihi</h1>
     <Footer />
     </>
  
