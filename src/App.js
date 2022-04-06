ba0bbb7a69607a1ec79db7c6ffe7554a6b83ac36
benharlucas <benhar.masahod@msugensan.edu.ph>
2022-04-06 09:32:00 +0800

    Second Day practice

--- src/App.js
+++ src/App.js
@@ -8,24 +8,39 @@ import Footer from './components/Footer'
 function App() {
   const firstname = 'Benhar'
   const middlename = 'Lucas'
-  const lastname = 'Dela Cruz';
+  const lastname = 'Masahod';
   const age = 24
-  const students = {
+  const students = 
+  [
+  {
     firstname: 'Benhar',
     middlename: 'Lucas',
     lastname: 'Dela Cruz',
     age: 20,
-  }
-  
-  const arr =[1,2,3,4,5]
-  
+  },
+  {
+   firstName: 'Juan',
+   middlename: 'Lopez',
+   lastname: 'Santa',
+   age: 25,
+  },
+  {
+  firstName: 'Sittie',
+  middlename: 'Mumina',
+  lastname: 'Veniegas',
+  age: 25,
+}
+
+]
+const arr = [1,2,3,4,5]
   return (
     <>
     <Header />
     <main>
-    <h1>Hello {firstname} {middlename}  {lastname} {students.age}</
-    h1>
-      {arr.map((num => <p> {num * 5} </p>))}
+    <h1>Hello {firstname} {middlename}  {lastname} {students.age}</h1>
+    {arr.map((num => <p> {num * 5} </p>))}
+    {students.map(( s => <p>{s.firstname}{s.middlename} {s.lastname}</p>))}
+    
       </main>
     <Footer />
     </>
--- src/components/footer.js
+++ src/components/footer.js
@@ -9,4 +9,4 @@ const footer = () => {
   )
 }
 
-export default Footer
+export default footer
